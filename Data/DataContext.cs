87db37e85e6e38018e439f3ac587e090fc4915f0
unknown <blesseddominic98@gmail.com>
2020-11-26 09:50:36 +0100

    second controller

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -8,6 +8,7 @@ namespace DatingApp.API.Data
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     { }
     public DbSet<WeatherForecast> Weathers { get; set; }
+    public DbSet<User> Users { get; set; }
     public DbSet<Weather> Weather { get; internal set; }
   }
 }
